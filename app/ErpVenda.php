92e67f9756613b4991c3f3842842ffae19408bb3
Luan Grabher <lujetix@hotmail.com>
2020-03-03 13:16:40 -0300

    ErpVenda -> add function for get json from erp

--- app/ErpVenda.php
+++ app/ErpVenda.php
@@ -2,9 +2,55 @@
 
 namespace App;
 
+use Goutte\Client;
 use Illuminate\Database\Eloquent\Model;
 
 class ErpVenda extends Model
 {
-    //
+    private function config()
+    {
+        return [
+            "css_nextPage" => ".PagedList-skipToNext",
+            "nameButtonLogin" => "Entrar",
+            "nameButtonSearch" => "Pesquisar",
+            "css_tableResult" => "#partialPedidoMensal"
+        ];
+    }
+
+    public function getJsonFromErp(int $start, int $end)
+    {
+        //Prepare function
+        $date_start = date("UTC",$start);
+        $date_end = date("UTC",$end);
+        $client = new Client();
+
+        //start navigation
+        $crawler = $client->request('GET', env("ERP_URL_HOME"));
+
+        // select the form and fill in some values
+        $form = $crawler->selectButton($this->config()['css_nextPage'])->form();
+        $form['UserName'] = env("ERP_USER");
+        $form['Password'] = env("ERP_PASSWORD");
+
+        // submit that form
+        $crawler = $client->submit($form);
+
+        //Extract data
+        $crawler = $client->request('GET', env("ERP_URL_REPORT"));
+        $form = $crawler->selectButton('Pesquisar')->form();
+        $form['dataInicio'] = $date_start;
+        $form['dataFinal'] = $date_end;
+        $crawler = $client->submit($form);
+
+        $htmlTable = $crawler->filter($this->config()['css_tableResult'])->html();
+
+        $uris = [];
+
+        $next = $crawler->filter($this->config()['css_nextPage']);
+        while ($next->count() > 0) {
+            $uris[] = $next->link()->getUri();
+        }
+
+        return $crawler->filter($this->config()['css_tableResult'])->html();
+    }
 }
--- app/Test.php
+++ app/Test.php
@@ -14,46 +14,4 @@ class Test extends Model
         return "Isso é uma data: " . $date->format("Y-m-d");
 
     }
-
-    public static function testPortalPonto()
-    {
-        $dateStart = "01/01/2020 00:00:00";
-        $dateFinal = "31/01/2020 00:00:00";
-
-        $uri_erp_home = "http://portaladm.pontodosconcursos.com.br";
-        $uri_erp_login = $uri_erp_home . "/Auth/Login";
-        $uri_er_report = $uri_erp_home . "/Pedido/RelatorioPedidoMensal";
-
-        $selector_nextPage = ".PagedList-skipToNext";
-
-        $user = "admin";
-        $pass = "@%159pdc";
-        $client = new Client();
-
-        $crawler = $client->request('GET', $uri_erp_login);
-
-        // select the form and fill in some values
-        $form = $crawler->selectButton('Entrar')->form();
-        $form['UserName'] = $user;
-        $form['Password'] = $pass;
-
-        // submit that form
-        $crawler = $client->submit($form);
-
-        //Extract data
-        $crawler = $client->request('GET', $uri_er_report);
-        $form = $crawler->selectButton('Pesquisar')->form();
-        $form['dataInicio'] = $dateStart;
-        $form['dataFinal'] = $dateFinal;
-        $crawler = $client->submit($form);
-
-        $htmlTable = $crawler->filter("#partialPedidoMensal")->html();
-
-        $next = $crawler->filter($selector_nextPage);
-        while($next->count() > 0){
-            $next->link()->getUri();
-        }
-
-        return $crawler->filter("#partialPedidoMensal")->html();
-    }
 }
