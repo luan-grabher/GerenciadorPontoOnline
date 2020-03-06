986bc38a5bfcd2f1662b0e6c942fc2cea08a1595
Luan Grabher <lujetix@hotmail.com>
2020-03-06 15:42:57 -0300

    having variable 'message' as 'messages'

--- resources/views/layouts/messages.blade.php
+++ resources/views/layouts/messages.blade.php
@@ -1,6 +1,6 @@
 @if(isset($messages) && is_array($messages))
     @foreach ($messages as $message)
-        @if(isset($messages['type']) && isset($messages['text']))
+        @if(isset($message['type']) && isset($message['text']))
             <div  class="alert alert-{{$message['type']}}">{{ $message['text'] }}</div>
         @endif
     @endforeach
