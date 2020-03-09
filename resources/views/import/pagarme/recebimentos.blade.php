66be9c13deb6334cc2de92cc017684a5ee933796
Luan Grabher <lujetix@hotmail.com>
2020-03-09 08:30:15 -0300

    New View Layout for import

--- resources/views/import/pagarme/recebimentos.blade.php
+++ resources/views/import/pagarme/recebimentos.blade.php
@@ -1,25 +1,5 @@
-@extends('layouts.onlyhead')
+@extends('layouts.import')
 
-@section('content')
-    <div class="col-10 mx-auto my-5 text-center">
-        @include('layouts.errors')
-        @include('layouts.messages')
-
-        {!! Form::open() !!}
-        <h3>Importar Pagarme Recebimentos</h3>
-        <div class="alert alert-info">Depois que clicar em 'Importar' a página poderá demorar um pouco para carregar. NÃO FECHE A ABA OU O NAVEGADOR NESTE MOMENTO.</div>
-        <div class="form-group">
-            {!! Form::label('Inicio:') !!}
-            {!! Form::date('inicio', \Carbon\Carbon::now(),['class'=>'form-control']) !!}
-        </div>
-        <div class="form-group">
-            {!! Form::label('Fim:') !!}
-            {!! Form::date('fim', \Carbon\Carbon::now(),['class'=>'form-control']) !!}
-        </div>
-        <div class="form-group">
-            {!! Form::submit('Importar',['class'=>'btn btn-success']) !!}
-        </div>
-        {!! Form::close() !!}
-    </div>
-
+@section('importName')
+    Pagarme Recebimentos
 @endsection
