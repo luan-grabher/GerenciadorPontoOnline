caa32076cbc4eaf65fc963597134c89ca52778ff
luan-grabher <lujetix@gmail.com>
2020-04-02 19:26:52 -0300

    Added import for Builder class in SaleItem.php

--- app/SaleItem.php
+++ app/SaleItem.php
@@ -3,14 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class SaleItem extends Model
 {
     protected function setKeysForSaveQuery(Builder $query)
     {
         $query->where([
-            ['product_id',$this->product_id],
-            ['sale_id',$this->sale_id]
+            ['product_id','=',$this->product_id],
+            ['sale_id','=',$this->sale_id]
         ]);
 
         return $query;
