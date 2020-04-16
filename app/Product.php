24fbd33e5524589a2202e61d2af8e22b38d7baa8
luan-grabher <lujetix@gmail.com>
2020-04-16 08:59:42 -0300

    Added product get profits.

--- app/Product.php
+++ app/Product.php
@@ -6,15 +6,93 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    public static function getProfits(\Datetime $startDateSale, \Datetime $endDateSale){
-        /*SELECT
-        sum(sale_items.value - sale_items.discount - sale_items.creditUsed - sale_items.creditAdded - sale_items.reversed) as lucro,
-        products.*
-        FROM products
-        inner join sale_items
-        on sale_items.product_id = products.id
-        group by products.id*/
+    protected static array $status = [
+        'canceled' => 'Cancelado',
+        'active' => 'Ativo'
+    ];
 
-        
+    public static function getProfits(\Datetime $startDateSale, \Datetime $endDateSale)
+    {
+        $profits = self::
+        select('products.id as product', 'products.name', 'products.value', 'products.dateStart', 'products.dateEnd', 'products.status', 'products.type')->
+        selectRaw(
+            'sum(sale_items.value - sale_items.discount - sale_items.creditUsed - sale_items.creditAdded - sale_items.reversed) as profit, '
+            . 'sum(sale_items.value)  as totalSalesValue, '
+            . 'sum(sale_items.discount) as totalSalesDiscount, '
+            . 'sum(sale_items.creditUsed) as totalSalesCreditUsed, '
+            . 'sum(sale_items.creditAdded) as totalSalesCreditAdded, '
+            . 'sum(sale_items.reversed) as totalSalesReversed'
+        )->
+        join('sale_items', 'sale_items.product_id', '=', 'products.id')->
+        join('sales', 'sale_items.sale_id', '=', 'sales.id')->
+        where([
+            ['sales.date', '>=', $startDateSale->format('Y-m-d')],
+            ['sales.date', '>=', $endDateSale->format('Y-m-d')]
+        ])->
+        groupBy('products.id')->
+        orderBy('products.dateStart')->
+        get();
+
+        foreach ($profits as $i => $profit){
+            #try{
+                $teachers = ProductTeacher::select('*')->where('product_id',$profit['product'])->get()->toArray();
+
+
+                $classes = array_sum (array_column ((array)$teachers, 'classes'));
+                try {
+                    $valueOneClass = $profit['profit'] / $classes;
+                }catch(\Exception $e){
+                }
+
+                $totalTeachers = 0;
+                foreach ($teachers as $teacher){
+                    #try{
+                        $totalTeachers += ($teacher['classes'] * $valueOneClass)/($teacher['percent']/100);
+                    #}catch(\Exception $e){
+                    #}
+                }
+
+                $profits[$i]['valueTeachers'] = $totalTeachers;
+                $profits[$i]['valueToAppropriate'] = $profit['profit'] - $totalTeachers;
+            #}catch(\Exception $e){
+            #}
+        }
+
+        return $profits;
+    }
+
+    public static function getCanceledProducts(\Datetime $startDateSale, \Datetime $endDateSale)
+    {
+        return self::
+        select('products.id', 'products.name', 'products.value', 'products.dateStart', 'products.dateEnd', 'products.status', 'products.type')->
+        where([
+            ['products.dateStart', '>=', $startDateSale->format('Y-m-d')],
+            ['products.dateEnd', '>=', $endDateSale->format('Y-m-d')],
+            ['products.status', '=', self::$status['canceled']]
+        ])->
+        groupBy('products.id')->
+        orderBy('products.dateStart')->
+        get();
+    }
+
+    public static function getStudents(\Datetime $startDateSale, \Datetime $endDateSale)
+    {
+        return self::
+        select(
+            'sale_items.product_id', 'products.name', 'products.dateStart', 'products.dateEnd', 'products.dateUnavailability', 'products.status'
+        )->selectRaw(
+            "count(if(sale_items.status = '". self::$status['active'] ."',1,NULL)) as active, " .
+            "count(if(sale_items.status <> '". self::$status['active'] ."',1,NULL)) as canceled, " .
+            "count(sale_items.status) as subscribers"
+        )->join(
+            'sale_items','products.id','sale_items.product_id'
+        )->join(
+            'sales','sales.id','sale_items.sale_id'
+        )->where([
+            ['sales.date','>=',$startDateSale],
+            ['sales.date','<=',$endDateSale]
+        ])->groupBy(
+            'product_id'
+        )->get();
     }
 }
--- app/Test.php
+++ app/Test.php
@@ -7,19 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Test extends Model
 {
     public static function test(){
-        return self::testGetCustomersCredits();
-    }
-
-    public static function testGetCustomersCredits(){
-        return Customer::getCustomersCredits();
-    }
-
-    public static function testImportERP(){
         $start = new \DateTime("2020-01-01");
         $end = new \DateTime("2020-01-31");
 
-        $import = new ImportSalesToDB($start,$end);
-
-        return $import->import();
+        return Product::getProfits($start,$end);
     }
 }
